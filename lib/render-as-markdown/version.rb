725155400b35488ab65e8dd44264e055a397fd74
Richard Metzler <richard@eaglefeed.me>
2013-10-13 02:39:39 +0200

    version 0.0.4

--- lib/render-as-markdown/version.rb
+++ lib/render-as-markdown/version.rb
@@ -1,6 +1,6 @@
 module RenderAsMarkdown
   GEM     = 'render-as-markdown'
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
 end
 
 #puts "This is version #{RenderAsMarkdown::VERSION} of gem #{RenderAsMarkdown::GEM}"
