0644b7b4af5b312c01ac127dc053e7b6cf777349
Richard Metzler <richard@eaglefeed.me>
2013-10-19 23:17:29 +0200

    try coverall

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -3,4 +3,7 @@ require "minitest/autorun"
 require "minitest/reporters"
 Minitest::Reporters.use!
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require_relative '../lib/render-as-markdown'
